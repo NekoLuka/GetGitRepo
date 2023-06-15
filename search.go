30bcd10d5966ea7fc884f6b78a56179403f0d0b6
Luka de Jong <52010222+NekoLuka@users.noreply.github.com>
2023-06-15 20:41:27 +0200

    added search function

--- search.go
+++ search.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
-type Snippets struct {
+type Snippet struct {
 	FilePath    string
 	MatchLength int
 	Matches     []int
@@ -43,6 +45,52 @@ func (s *Search) index() {
 	s.log.Info("Successfully indexed directory")
 }
 
-func (s *Search) search(searchQuery []byte) {
+func (s *Search) search(searchQuery []byte) []Snippet {
+	var snippets []Snippet
 
+	// Loop over the files indexed by the index function
+	for _, v := range s.articles {
+		file, err := os.Open(v)
+		if err != nil {
+			s.log.Error(err, false)
+			file.Close()
+			continue
+		}
+		info, err := file.Stat()
+		if err != nil {
+			s.log.Error(err, false)
+			file.Close()
+			continue
+		}
+
+		// Create the byte array to save the bytes
+		data := make([]byte, info.Size())
+		_, err = file.Read(data)
+		if err != nil {
+			s.log.Error(err, false)
+			file.Close()
+			continue
+		}
+
+		if !bytes.Contains(data, searchQuery) {
+			file.Close()
+			continue
+		}
+
+		// Loop through the file while saving the location of matches
+		snippet := Snippet{FilePath: v, MatchLength: len(searchQuery)}
+		index := 0
+		for true {
+			index = bytes.Index(data, searchQuery)
+			if index == -1 {
+				break
+			}
+			snippet.Matches = append(snippet.Matches, index)
+			data = data[index+1:]
+		}
+
+		snippets = append(snippets, snippet)
+		file.Close()
+	}
+	return snippets
 }
